8b28781229ec1d2210e3e515a900e1c5f946409a
Zhentian Wan <answer881215@gmail.com>
2020-04-05 16:20:16 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { LoadingSkeletonService } from 'projects/loading-skeleton/src/public-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LoadingSkeletonService]
 })
 export class AppComponent {
-  title = 'ngx-loading-skeleton';
+  title = 'libs-workspaces';
+  isDark: boolean = false;
+  isVisible;
+  theme = {
+    light: {
+      backgroundColor: 'lightblue'
+    },
+    dark: {
+      backgroundColor: 'darkblue'
+    }
+  };
+  themeMode = 'light';
+  constructor(private loadingService: LoadingSkeletonService) {
+    this.loadingService.show();
+    this.isVisible = false;
+  }
+
+  ngOnInit() {}
+
+  toggleDark() {
+    this.isDark = !this.isDark;
+    this.loadingService.changeMode(this.isDark);
+  }
+
+  cmpToggleMode() {
+    this.themeMode = this.themeMode === 'light' ? 'dark' : 'light';
+  }
 }

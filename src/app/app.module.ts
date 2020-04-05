96cddf338e7fe60a3d06e2453619fd986e64d775
Zhentian Wan <answer881215@gmail.com>
2020-04-05 16:20:33 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { NdwLoadingSkeletonModule } from 'projects/loading-skeleton/src/public-api';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    NdwLoadingSkeletonModule
+    /*LoadingSkeletonModule.forRoot({
+      duration: "2s",
+      theme: {
+        light: {
+          backgroundColor: "pink"
+        },
+        dark: {
+          backgroundColor: "red"
+        }
+      }
+    })*/
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

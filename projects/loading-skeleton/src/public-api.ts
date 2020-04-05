d34e2322f295a5a1177ef3826a1d2b3bdbeb7af8
Zhentian Wan <answer881215@gmail.com>
2020-04-05 16:21:59 +0300

    Update public-api.ts

--- projects/loading-skeleton/src/public-api.ts
+++ projects/loading-skeleton/src/public-api.ts
@@ -5,3 +5,13 @@
 export * from './lib/loading-skeleton.service';
 export * from './lib/loading-skeleton.component';
 export * from './lib/loading-skeleton.module';
+export * from './lib/loading-skeleton.config';
+
+export * from './lib/loading-placeholder/loading-placeholder.component';
+
+export * from './lib/template-components/loading-button/loading-button.component';
+export * from './lib/template-components/loading-headline/loading-headline.component';
+export * from './lib/template-components/loading-image/loading-image.component';
+export * from './lib/template-components/loading-list/loading-list.component';
+export * from './lib/template-components/loading-text/loading-text.component';
+export * from './lib/template-components/loading-textarea/loading-textarea.component';
